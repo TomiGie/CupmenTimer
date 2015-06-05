7952439490d9b17d87899bcbc098a8484b9cd74c
津田 恭平 <shimano.entou@gmail.com>
2015-06-05 16:52:09 +0900

    タイマーアプリに必要なTextViewとButtonを追加

--- app/src/main/java/org/tohokutechdojo/cupmentimer/MainActivity.java
+++ app/src/main/java/org/tohokutechdojo/cupmentimer/MainActivity.java
@@ -2,16 +2,37 @@ package org.tohokutechdojo.cupmentimer;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTimeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTimeTextView = (TextView)findViewById(R.id.time_textView);
+
+        Button startButton = (Button) findViewById(R.id.start_button);
+        startButton.setOnClickListener(this);
+
+        Button stopButton = (Button) findViewById(R.id.stop_button);
+        stopButton.setOnClickListener(this);
+
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.start_button:
+                break;
+            case R.id.stop_button:
+                break;
+        }
+    }
 }
